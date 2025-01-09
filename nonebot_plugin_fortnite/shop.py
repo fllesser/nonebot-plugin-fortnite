4614886123917c731406106d0b7c7839566b6621
fllesser <fllessive@gmail.com>
2025-01-09 11:32:40 +0800

    update shop.py

--- nonebot_plugin_fortnite/shop.py
+++ nonebot_plugin_fortnite/shop.py
@@ -19,7 +19,7 @@ async def screenshot_shop_img() -> Path:
             # page = await browser.new_page()
             context = await browser.new_context(
                 extra_http_headers = {
-                  'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+                  'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
                   'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                   'Accept-Encoding': "gzip, deflate",
                   'upgrade-insecure-requests': "1",
