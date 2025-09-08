73b8f4308cda3afce6d4afa96f3c235dca83ebdc
fllesser <fllessive@gmail.com>
2025-09-08 21:32:22 +0800

    🐎 ci: codecov, ruff, typos

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -1,29 +1,7 @@
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, Message, MessageSegment
+from fake import fake_group_message_event_v11
+from nonebot.adapters.onebot.v11 import Bot, Message, MessageSegment
 from nonebug import App
 import pytest
-
-
-def make_onebot_msg(message: Message) -> GroupMessageEvent:
-    from time import time
-
-    from nonebot.adapters.onebot.v11.event import Sender
-
-    event = GroupMessageEvent(
-        time=int(time()),
-        sub_type="normal",
-        self_id=123456,
-        post_type="message",
-        message_type="group",
-        message_id=12345623,
-        user_id=1234567890,
-        group_id=1234567890,
-        raw_message=message.extract_plain_text(),
-        message=message,
-        original_message=message,
-        sender=Sender(),
-        font=123456,
-    )
-    return event
 
 
 @pytest.mark.asyncio
@@ -44,14 +22,14 @@ async def test_vb_img(app: App):
         vb_file.unlink()
 
     texts = ["vb图", "VB图", "Vb图"]
-    msgs = [make_onebot_msg(Message(text)) for text in texts]
+    msg_events = [fake_group_message_event_v11(message=text) for text in texts]
 
     async with app.test_matcher(vb_matcher) as ctx:
         adapter = nonebot.get_adapter(OnebotV11Adapter)
         bot = ctx.create_bot(base=Bot, adapter=adapter)
-        for msg in msgs:
-            ctx.receive_event(bot, msg)
-            ctx.should_call_send(msg, Message(MessageSegment.image(vb_file)), result=None, bot=bot)
+        for event in msg_events:
+            ctx.receive_event(bot, event)
+            ctx.should_call_send(event, Message(MessageSegment.image(vb_file)), result=None, bot=bot)
             ctx.should_finished()
 
     assert vb_file.exists()
@@ -68,19 +46,19 @@ async def test_shop_img(app: App):
         shop_file.unlink()
 
     texts = ["商城", "商城。。。。"]
-    msgs = [make_onebot_msg(Message(text)) for text in texts]
+    msg_events = [fake_group_message_event_v11(message=text) for text in texts]
 
     async with app.test_matcher(shop_matcher) as ctx:
         adapter = nonebot.get_adapter(OnebotV11Adapter)
         bot = ctx.create_bot(base=Bot, adapter=adapter)
-        for msg in msgs:
-            ctx.receive_event(bot, msg)
+        for event in msg_events:
+            ctx.receive_event(bot, event)
             # assert shop_file.exists()
             should_send = (
                 MessageSegment.image(shop_file) + "可前往 https://www.fortnite.com/item-shop?lang=zh-Hans 购买"
             )
             ctx.should_call_send(
-                msg,
+                event,
                 Message(should_send),
                 result=None,
                 bot=bot,
@@ -102,7 +80,7 @@ async def test_stats_matcher(app: App):
         pytest.skip("api_key 未设置，跳过测试")
 
     texts = ["战绩 红桃QAQ", "生涯战绩 红桃QAQ"]
-    msg_events = [make_onebot_msg(Message(text)) for text in texts]
+    msg_events = [fake_group_message_event_v11(message=text) for text in texts]
 
     async with app.test_matcher(stats_alc) as ctx:
         adapter = nonebot.get_adapter(OnebotV11Adapter)
